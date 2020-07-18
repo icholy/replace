7cac3ed1a5f18d56d4397d13fd564e625bc0fd97
Ilia Choly <ilia.choly@gmail.com>
2020-07-18 13:57:05 -0400

    combine all errors

--- replace_test.go
+++ replace_test.go
@@ -16,22 +16,89 @@ func TestTransformer(t *testing.T) {
 		in, out string
 		tr      transform.Transformer
 	}{
-		{"test", "test", String("", "x")},
-		{"a", "b", String("a", "b")},
-		{"yes", "no", String("yes", "no")},
-		{"what what what", "wut wut wut", String("what", "wut")},
-		{"???????", "*******", String("?", "*")},
-		{"no matches", "no matches", String("x", "y")},
-		{"hello", "heLLo", String("l", "L")},
-		{"hello", "hello", String("x", "X")},
-		{"", "", String("x", "X")},
-		{"radar", "<r>ada<r>", String("r", "<r>")},
-		{"banana", "b<>n<>n<>", String("a", "<>")},
-		{"banana", "b<><>a", String("an", "<>")},
-		{"banana", "b<>na", String("ana", "<>")},
-		{"banana", "banana", String("a", "a")},
-		{"xxx", "", String("x", "")},
-		{strings.Repeat("foo_", 8<<10), strings.Repeat("bar_", 8<<10), String("foo", "bar")},
+		{
+			in: "test", out: "test",
+			tr: String("", "x"),
+		},
+		{
+			in: "a", out: "b",
+			tr: String("a", "b"),
+		},
+		{
+			in: "yes", out: "no",
+			tr: String("yes", "no"),
+		},
+		{
+			in: "what what what", out: "wut wut wut",
+			tr: String("what", "wut"),
+		},
+		{
+			in: "???????", out: "*******",
+			tr: String("?", "*"),
+		},
+		{
+			in: "no matches", out: "no matches",
+			tr: String("x", "y"),
+		},
+		{
+			in: "hello", out: "heLLo",
+			tr: String("l", "L"),
+		},
+		{
+			in: "hello", out: "hello",
+			tr: String("x", "X"),
+		},
+		{
+			in: "", out: "",
+			tr: String("x", "X"),
+		},
+		{
+			in: "radar", out: "<r>ada<r>",
+			tr: String("r", "<r>"),
+		},
+		{
+			in: "banana", out: "b<>n<>n<>",
+			tr: String("a", "<>"),
+		},
+		{
+			in: "banana", out: "b<><>a",
+			tr: String("an", "<>"),
+		},
+		{
+			in: "banana", out: "b<>na",
+			tr: String("ana", "<>"),
+		},
+		{
+			in: "banana", out: "banana",
+			tr: String("a", "a"),
+		},
+		{
+			in: "xxx", out: "",
+			tr: String("x", ""),
+		},
+		{
+			in: strings.Repeat("foo_", 8<<10), out: strings.Repeat("bar_", 8<<10),
+			tr: String("foo", "bar"),
+		},
+		{
+			in: "a", out: "b",
+			tr: RegexpString(regexp.MustCompile("a"), "b"),
+		},
+		{
+			in: "testing", out: "x",
+			tr: RegexpString(regexp.MustCompile(".*"), "x"),
+		},
+		{
+			in: strings.Repeat("--ax-- --bx--", 4<<10), out: strings.Repeat("--xx-- --xx--", 4<<10),
+			tr: RegexpString(regexp.MustCompile(`--\wx--`), "--xx--"),
+		},
+		{
+			in: strings.Repeat("--1x-- --2x-- --3x--", 8<<10), out: strings.Repeat("--0x-- --1x-- --2x--", 8<<10),
+			tr: RegexpStringSubmatchFunc(regexp.MustCompile(`--(\d)x--`), func(match []string) string {
+				x, _ := strconv.Atoi(match[1])
+				return fmt.Sprintf("--%dx--", x-1)
+			}),
+		},
 	}
 	for i, tt := range tests {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
@@ -40,39 +107,4 @@ func TestTransformer(t *testing.T) {
 			assert.DeepEqual(t, result, tt.out)
 		})
 	}
-}
-
-func TestRegex(t *testing.T) {
-	tests := []struct {
-		in  string
-		re  string
-		new string
-		out string
-	}{
-		{"a", "a", "b", "b"},
-		{"testing", ".*", "x", "x"},
-		{strings.Repeat("--ax-- --bx--", 4<<10), `--\wx--`, "--xx--", strings.Repeat("--xx-- --xx--", 4<<10)},
-	}
-	for i, tt := range tests {
-		t.Run(strconv.Itoa(i), func(t *testing.T) {
-			re := regexp.MustCompile(tt.re)
-			tr := RegexpString(re, tt.new)
-			result, _, err := transform.String(tr, tt.in)
-			assert.NilError(t, err)
-			assert.DeepEqual(t, result, tt.out)
-		})
-	}
-}
-
-func TestRegexStringSubmatchFunc(t *testing.T) {
-	in := strings.Repeat("--1x-- --2x-- --3x--", 8<<10)
-	re := regexp.MustCompile(`--(\d)x--`)
-	tr := RegexpStringSubmatchFunc(re, func(match []string) string {
-		x, _ := strconv.Atoi(match[1])
-		return fmt.Sprintf("--%dx--", x-1)
-	})
-	result, _, err := transform.String(tr, in)
-	assert.NilError(t, err)
-	want := strings.Repeat("--0x-- --1x-- --2x--", 8<<10)
-	assert.DeepEqual(t, result, want)
 }
